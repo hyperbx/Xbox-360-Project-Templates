a120126da946c5a686665c6b5d00f8a4150ac820
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-03-05 04:25:26 +0000

    Indentation and consistency

--- Xbox 360 DashLaunch Plugin/DllMain.cpp
+++ Xbox 360 DashLaunch Plugin/DllMain.cpp
@@ -1,5 +1,12 @@
+// DllMain.cpp : Defines the entry point for the DLL application.
+//
+
 #include "stdafx.h"
 
+//-------------------------------------------------------------------------------------
+// Name: DllMain()
+// Desc: The DLL application's entry point
+//-------------------------------------------------------------------------------------
 BOOL APIENTRY DllMain(HMODULE in_module, DWORD in_reason, LPVOID in_reserved)
 {
 	switch (in_reason)
